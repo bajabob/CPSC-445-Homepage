9889a137594e618e07045907d84ff13cfbfcb344
Bob Timm <bajabob.tx@gmail.com>
2016-01-24 13:46:36 -0600

    Updating config to match prod environment.

--- include/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bajabob',
-    'username' => 'bajabob',
-    'password' => 'r3propeller',
-    'charset' => 'utf8',
-];
---
+++ include/config/template.db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=NAME',
+    'username' => 'USER',
+    'password' => 'PASS',
+    'charset' => 'utf8',
+];
