cdce78da51d8df40171ad146eaaab757db7c7b27
Bob Timm <bajabob.tx@gmail.com>
2016-01-24 22:27:56 -0600

    a1 stable, updating copyright

--- include/views/layouts/main.php
+++ include/views/layouts/main.php
@@ -104,7 +104,7 @@ AppAsset::register($this);
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        Copyright &copy; Your Website 2014
+                        Copyright &copy; Robert Timm 2016
                     </div>
                 </div>
             </div>
